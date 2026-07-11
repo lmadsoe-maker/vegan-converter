4a4bac67154cb1274c151e916d8091c6bc98de28
Laila Madsø <lmadsoe@gmail.com>
2026-07-11 14:56:36 +0000

    build: rebuild frontend with API_PATH fix for 422 errors

---
+++ frontend/dist/assets/SomethingWentWrongPage-f64d733b.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-e3abb529.js";import{P as t}from"./ProdErrorPage-8985f3e8.js";function n(){return r.jsx(t,{text:"Something went wrong.",canRefresh:!0})}export{n as default};
